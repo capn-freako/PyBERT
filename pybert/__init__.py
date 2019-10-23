0a4c1193d3a0a9d6354db31599af18273e47f608
David Banas <david@luminouscomputing.com>
2019-10-23 13:36:14 -0700

    v3.1.2

--- pybert/__init__.py
+++ pybert/__init__.py
@@ -47,6 +47,6 @@ The application source is divided among several files, as follows:
 Copyright (c) 2014 by David Banas; All rights reserved World wide.
 """
 __version__ = "3.1.2"
-__date__ = "October 21, 2019"
+__date__ = "October 23, 2019"
 __authors__ = "David Banas & David Patterson"
 __copy__ = "Copyright (c) 2014 David Banas, 2019 David Patterson"
