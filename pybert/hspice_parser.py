43d34486d64d4376f53b21b7c2d306fc655dff52
David Banas <david@luminouscomputing.com>
2022-01-08 05:56:30 -0800

    Added a parser for HSPICE output waveforms.

---
+++ pybert/hspice_parser.py
@@ -0,0 +1,94 @@
+"""
+HSPICE CSDF waveform file parser.
+
+Original author: David Banas <capn.freako@gmail.com>
+
+Original date:   January 7, 2022
+
+Copyright (c) 2022 David Banas; all rights reserved World wide.
+"""
+import re
+# from parsec import ParseError, generate, many, many1, regex, string, \
+#                    parsecmap, bind, none_of
+from parsec import *
+
+class CSDF:
+    """Common Simulation Data Format (CSDF)
+    """
+    def __init__(self, hdr, nms, wvs):
+        assert(len(nms) == int(hdr['NODES']))
+        self.header = {}
+        self.header.update(hdr)
+        self.names  = nms
+        self.waves  = wvs
+
+# CSDF grammar definition
+
+# ignore cases.
+whitespace = regex(r"\s+", re.MULTILINE)
+# comment    = regex(r"^\*.*|\$.*")
+# ignore     = many((whitespace | comment))
+ignore     = many(whitespace)
+
+def lexeme(p):
+    """Lexer for words."""
+    return p << ignore  # skip all ignored characters.
+
+def flag(ch):
+    """Lexer for CSDF flags."""
+    return lexeme(string("#") >> string(ch))
+
+symbol     = lexeme(regex(r"[a-zA-Z_]+"))
+number     = lexeme(regex(r"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?"))
+nat        = lexeme(regex(r"[0-9]+"))
+slash      = lexeme(string("/"))
+
+@generate("CSDF complex number")
+def csdr_num():
+    "Parse CSDF complex number."
+    xs = yield sepBy1(number, slash)
+    if len(xs) > 1:
+        return complex(len[0], len[1])
+    else:
+        return xs[0]
+
+def val_samps(n):
+    """Parser for CSDF value samples line."""
+    return count(csdr_num, int(n))
+
+csdr_str   = lexeme(string("'") >> many(none_of("'")) << string("'"))
+# kv_pair    = lexeme(symbol << string("=") + csdr_str)
+kv_pair    = (symbol << string("=")) + csdr_str
+# header     = lexeme(flag("H") >> many(kv_pair))
+header     = flag("H") >> many(kv_pair)
+sig_names  = lexeme(flag("N") >> many(csdr_str))
+# wave_samps = lexeme((flag("C") >> number) + (nat >>= val_samps))
+wave_samps = lexeme((flag("C") >> number) + (nat.bind(val_samps)))
+# waves      = many(wave_samps)
+
+# beg_wave = lexeme()
+# lparen = lexeme(string("("))
+# rparen = lexeme(string(")"))
+# integ  = lexeme(regex(r"[-+]?[0-9]+"))
+# tap_ix = integ.parsecmap(int2tap)
+# true = lexeme(string("True")).result(True)
+# false = lexeme(string("False")).result(False)
+# ami_string = lexeme(regex(r'"[^"]*"'))
+
+# atom = number | symbol | ami_string | (true | false)
+# node_name = symbol | tap_ix  # `tap_ix` is new and gives the tap position; negative positions are allowed.
+
+@generate("CSDF data")
+def csdf_data():
+    "Parse CSDF file contents."
+    hdr = yield header
+    nms = yield sig_names
+    wvs = yield many1(wave_samps)
+    return CSDF(
+        dict(map(lambda pr: (pr[0], "".join(pr[1])), hdr)),
+        list(map(lambda cs: "".join(cs), nms)),
+        list(map(lambda pr: (float(pr[0]), list(map(float, pr[1]))), wvs)))
+
+
+# expr = atom | node
+# ami_defs = ignore >> node
