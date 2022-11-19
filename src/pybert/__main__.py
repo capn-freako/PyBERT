a4647f47a98c57f9a0cc5d9d95eea0675d3444c7
David Patterson <14203439+jdpatt@users.noreply.github.com>
2022-11-19 15:10:43 -0500

    doc: typos

--- src/pybert/__main__.py
+++ src/pybert/__main__.py
@@ -1,7 +1,7 @@
 """Main entry into the PyBERT GUI when called with python -m.
 
-This is will now largely be for debug or if users want to use the python
--m option since calling `pybert` will point to cli.py.
+This is now largely for debug or if users want to use the python
+-m option since calling `pybert` will instead point to cli.py.
 """
 from pybert.gui.view import traits_view
 from pybert.pybert import PyBERT
