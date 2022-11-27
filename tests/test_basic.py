86fe2c38916171a0c813de6ba9410486c2c2df91
David Patterson <14203439+jdpatt@users.noreply.github.com>
2022-11-27 14:18:27 -0500

    fix: use pytest fixtures over attribute and remove version test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,60 +1,54 @@
 """Run some basic tests on a PyBERT instance."""
 import numpy as np
-
-import pybert
-from pybert.pybert import PyBERT
+import pytest
 
 
+@pytest.mark.usefixtures("dut")
 class TestBasic(object):
     """Basic tests of a properly initialized PyBERT."""
 
-    dut = PyBERT(gui=False)
-
-    def test_version(self):
-        assert pybert.__version__ == "3.5.8"
-
-    def test_status(self):
+    def test_status(self, dut):
         """Test post-simulation status."""
-        assert self.dut.status == "Ready.", "Status not 'Ready.'!"
+        assert dut.status == "Ready.", "Status not 'Ready.'!"
 
-    def test_perf(self):
+    def test_perf(self, dut):
         """Test simulation performance."""
-        assert self.dut.total_perf > (1e6 / 60), "Performance dropped below 1 Msmpls/min.!"
+        assert dut.total_perf > (1e6 / 60), "Performance dropped below 1 Msmpls/min.!"
 
-    def test_ber(self):
+    def test_ber(self, dut):
         """Test simulation bit errors."""
-        assert not self.dut.bit_errs, "Bit errors detected!"
+        assert not dut.bit_errs, "Bit errors detected!"
 
-    def test_dly(self):
+    def test_dly(self, dut):
         """Test channel delay."""
-        assert self.dut.chnl_dly > 1e-9 and self.dut.chnl_dly < 10e-9, "Channel delay is out of range!"
+        assert dut.chnl_dly > 1e-9 and dut.chnl_dly < 10e-9, "Channel delay is out of range!"
 
-    def test_isi(self):
+    def test_isi(self, dut):
         """Test ISI portion of jitter."""
-        assert self.dut.isi_dfe < 50e-12, "ISI is too high!"
+        assert dut.isi_dfe < 50e-12, "ISI is too high!"
 
-    def test_dcd(self):
+    def test_dcd(self, dut):
         """Test DCD portion of jitter."""
-        assert self.dut.dcd_dfe < 20e-12, "DCD is too high!"
+        assert dut.dcd_dfe < 20e-12, "DCD is too high!"
 
-    def test_pj(self):
+    def test_pj(self, dut):
         """Test periodic portion of jitter."""
-        assert self.dut.pj_dfe < 20e-12, "Periodic jitter is too high!"
+        assert dut.pj_dfe < 20e-12, "Periodic jitter is too high!"
 
-    def test_rj(self):
+    def test_rj(self, dut):
         """Test random portion of jitter."""
-        assert self.dut.rj_dfe < 20e-12, "Random jitter is too high!"
+        assert dut.rj_dfe < 20e-12, "Random jitter is too high!"
 
-    def test_lock(self):
+    def test_lock(self, dut):
         """Test CDR lock, by ensuring that last 20% of locked indication vector
         is all True."""
-        _lockeds = self.dut.lockeds
+        _lockeds = dut.lockeds
         assert all(_lockeds[4 * len(_lockeds) // 5 :]), "CDR lock is unstable!"
 
-    def test_adapt(self):
+    def test_adapt(self, dut):
         """Test DFE lock, by ensuring that last 20% of all coefficient vectors
         are stable to within +/-20% of their mean."""
-        _weights = self.dut.adaptation  # rows = step; cols = tap
+        _weights = dut.adaptation  # rows = step; cols = tap
         _ws = np.array(list(zip(*_weights[4 * len(_weights) // 5 :])))  # zip(*x) = unzip(x)
         _means = list(map(lambda xs: sum(xs) / len(xs), _ws))
         assert all(
