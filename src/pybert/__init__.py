7012ee9865f5f40a3301307beb25c1da8b999eba
David Banas <capn.freako@gmail.com>
2023-03-15 17:41:19 -0400

    PyBERT v4.1.0 release candidate.

--- src/pybert/__init__.py
+++ src/pybert/__init__.py
@@ -10,11 +10,14 @@ Testing by:      Mark Marlett <mark.marlett@gmail.com>
 
 Copyright (c) 2014 by David Banas; All rights reserved World wide.
 """
-__version__ = "4.0.6"
-__date__ = "February 13, 2023"
+__version__ = "4.1.0"
+__date__ = "March 15, 2023"
 __authors__ = "David Banas & David Patterson"
 __copy__ = "Copyright (c) 2014 David Banas, 2019 David Patterson"
 
+# You can play with the different alternatives below,
+# to try and optimize the visual appearance of PyBERT plots.
+# You should only have one of the "ETSConfig.toolkit = ..." lines uncommented.
 # fmt: off
 # isort: off
 from traits.etsconfig.api import ETSConfig
