472a3532bf131f00d25e56a8a1dfc6390be91419
David Banas <capn.freako@gmail.com>
2025-04-30 05:49:06 -0400

    Added explicit double-quotes around AMI parameters of type: String.
    Fixes #163

--- type_stubs/pyibisami/tools/run_notebook.pyi
+++ type_stubs/pyibisami/tools/run_notebook.pyi
@@ -1,8 +1,8 @@
 from _typeshed import Incomplete
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any
 
 NOTEBOOK: Incomplete
 
-def run_notebook(ibis_file: Path, notebook: Path, out_dir: Optional[Path] = ..., notebook_params: Optional[dict[str, Any]] = ...) -> None: ...
+def run_notebook(ibis_file: Path, notebook: Path, out_dir: Path | None = None, notebook_params: dict[str, Any] | None = None) -> None: ...
 def main(notebook, out_dir, params, ibis_file, bit_rate, debug, is_tx, nspui, nbits, plot_t_max, f_max, f_step, fig_x, fig_y) -> None: ...
