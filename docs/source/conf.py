ca4576f2362fe20aff3dbd2eff66125931513ffb
David Banas <capn.freako@gmail.com>
2025-07-30 16:35:09 -0400

    Added explicit version and release tags to docs/source/conf.py.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,6 +82,8 @@ master_doc = "index"
 project = "PyBERT"
 copyright = "2014 David Banas, 2019 David Patterson"
 author = "David Banas & David Patterson"
+version = "v9.1"
+release = "v9.1.3"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
