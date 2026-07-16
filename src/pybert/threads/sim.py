f40494809673abc630ec71e456fc2321f3f0ce15
David Banas <david.banas@keysight.com>
2026-07-16 04:49:50 -0400

    Added `initial_run` and `update_plots` keyword arguments to `RunSimThread`.
    - This change allows script based users to control the PyBERT run mode,
    while still being able to check/report status periodically.
    
    - Bumped minor version number.

--- src/pybert/threads/sim.py
+++ src/pybert/threads/sim.py
@@ -8,14 +8,32 @@ class RunSimThread(StoppableThread):
     """Used to run the simulation in its own thread, in order to preserve GUI
     responsiveness."""
 
-    def __init__(self):
+    def __init__(self, initial_run: bool = False, update_plots: bool = True):
+        """
+        Keyword Args:
+            initial_run: If True, don't update the eye diagrams, since
+                they haven't been created, yet.
+                Default: False
+            update_plots: If True, update the plots, after simulation
+                completes. This option can be used by larger scripts, which
+                import *pybert*, in order to avoid graphical back-end
+                conflicts and speed up this function's execution time.
+                Default: True
+        """
         super().__init__()
         self.the_pybert = None
+        self.initial_run = initial_run
+        self.update_plots = update_plots
 
     def run(self):
         """Run the simulation(s)."""
         try:
-            my_run_simulation(self.the_pybert, aborted_sim=self.stopped)
+            my_run_simulation(
+                self.the_pybert,
+                initial_run=self.initial_run,
+                update_plots=self.update_plots,
+                aborted_sim=self.stopped
+            )
         except RuntimeError as err:
             print(f"Error in `pybert.threads.sim.RunSimThread`: {err}")
             raise
